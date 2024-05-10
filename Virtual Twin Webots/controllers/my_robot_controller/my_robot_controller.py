58d0f4a948badec8b7de56292f8480369aa628c3
tjallingk <tkaastra@gmx.com>
2024-05-10 16:44:03 +0200

    kinmatics are working

--- Virtual Twin Webots/controllers/my_robot_controller/my_robot_controller.py
+++ Virtual Twin Webots/controllers/my_robot_controller/my_robot_controller.py
@@ -31,36 +31,24 @@ def calculate_triangle_angles(distance, L1, L2):
 
     return math.degrees(theta1), math.degrees(theta2), math.degrees(theta3)
 
-
-
-def inverse_kinematics(x, y, L1, L2):
-    r = math.sqrt(x**2 + y**2)
-
-    if r > L1 + L2 or r < abs(L1 - L2):
-        raise ValueError("Point is out of reach")
-
-    cos_theta2 = (r**2 - L1**2 - L2**2) / (2 * L1 * L2)
-    theta2 = math.acos(cos_theta2)
-
-    alpha = math.atan2(y, x)
-    beta = math.atan2(L2 * math.sin(theta2), L1 + L2 * math.cos(theta2))
-    theta1 = alpha - beta
-
-    return math.degrees(theta1), math.degrees(theta2)
     
 def calculate_joint_angles(distance, L1, L2, angle):
     theta1, theta2, theta3 = calculate_triangle_angles(distance, L1, L2)
-
-    # Convert theta2 and theta3 to radians
-    jointbase = math.radians(theta2 + angle)
-    jointmiddle = math.radians(0-180+theta3)
+    if theta3 > 0:
+        # Convert theta2 and theta3 to radians
+        jointbase = math.radians(theta2 + angle)
+        jointmiddle = math.radians(0-180+theta3)
     
-    # Check if angle for jointbase is more than 180 degrees
-    if jointbase > math.pi:
-        jointbase -= 2 * math.pi
+        # Check if angle for jointbase is more than 180 degrees
+        if jointbase > math.pi:
+            jointbase -= 2 * math.pi
     
-    return jointbase, jointmiddle, theta1, theta2, theta3
-    
+        return jointbase, jointmiddle, theta1, theta2, theta3
+    else:
+        # no angle for middle joint is the distance to large or small?
+        jointbase = math.radians(theta2 + angle)
+        jointmiddle = math.radians(theta3)
+        return jointbase, jointmiddle, theta1, theta2, theta3
 # Create a robot instance
 robot = Robot()
 
@@ -74,7 +62,7 @@ L2 = 300  # Length of the second arm
 
 # Example target Cartesian coordinates
 x = 200
-y = 0
+y = 200
     
 distance, angle = calculate_distance_and_angle(x, y)
 print("Distance from center in straight line:", distance)
