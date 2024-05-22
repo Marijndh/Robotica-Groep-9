2e1ea1dba72a019cb180d064fe7d493f89219d53
tjallingk <tkaastra@gmx.com>
2024-05-22 15:02:34 +0200

    more ax12 controller trying

--- ros/servo controller/ros2_ws/src/my_ax12_servo_controller/my_ax12_servo_controller/my_ax12_node.py
+++ ros/servo controller/ros2_ws/src/my_ax12_servo_controller/my_ax12_servo_controller/my_ax12_node.py
@@ -2,124 +2,130 @@ import rclpy
 from rclpy.node import Node
 import serial
 import RPi.GPIO as GPIO
-import time
 from std_msgs.msg import Float64
 
 class DynamixelController(Node):
     def __init__(self):
         super().__init__('dynamixel_controller')
-
-        # Initialize GPIO pin for direction control
         self.direction_pin = 18
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.direction_pin, GPIO.OUT)
+        GPIO.setwarnings(False)
 
-        # Initialize serial port
         self.serial_port = serial.Serial('/dev/ttyS0', baudrate=1000000, timeout=1)
         
-        # Dynamixel protocol constants
+        self.ADDR_MX_OPERATING_MODE = 11#incorrect should be the adress of clockwise moving
         self.ADDR_MX_GOAL_POSITION = 30
+        self.ADDR_MX_MOVING_SPEED = 32   # Address for setting moving speed
         self.ADDR_MX_PRESENT_POSITION = 36
         self.PROTOCOL_VERSION = 1.0
-        self.DXL_ID = 3  # Change to your servo ID
-        self.BAUDRATE = 1000000
-        self.DEVICENAME = '/dev/ttyS0'
+        self.DXL_ID = 5
         
-        # Create a publisher to publish the position
-        self.pub = self.create_publisher(Float64, 'servo_position', 10)
-        
-        # Set initial position
-        self.set_position(512)
-        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.create_subscription(Float64, 'servo_position', self.listener_callback, 10)
+        self.get_logger().info('Dynamixel Controller Node has been started')
+        #self.set_normal_mode()
 
-    def set_position(self, position):
-        # Create a packet to set the goal position
-        packet = self.build_packet(self.DXL_ID, self.ADDR_MX_GOAL_POSITION, position)
+    #def set_normal_mode(self):
+        # Set the servo to normal mode
+        #packet = self.build_packet(self.DXL_ID, self.ADDR_MX_OPERATING_MODE, 0)
+        #self.send_packet(packet)
+
+    def move_for_duration(self, speed, duration):
+    # Set the moving speed
+    packet = self.build_packet(self.DXL_ID, self.ADDR_MX_MOVING_SPEED, speed)
+    self.send_packet(packet)
+
+    # Wait for the specified duration
+    time.sleep(duration)
+
+    # Stop the servo
+    self.stop()
+
+    def listener_callback(self, msg):
+        position = int(msg.data)
+        if 0 <= position <= 1023:
+            self.get_logger().info(f'Received position command: {position}')
+            self.set_position(position)
+        else:
+            self.get_logger().error(f'Invalid position value: {position}')
+    def degrees_to_position(self, degrees):
+        if 0 <= degrees <= 300:
+            return int(degrees * 1023 / 300)
+        else:
+            self.get_logger().error('Degrees out of range (0-300)')
+            return None
+    
+    def set_position_degrees(self, degrees):
+        position = self.degrees_to_position(degrees)
+        if position is not None:
+            self.set_position(position)
         
-        # Send the packet
+    def set_position(self, position):
+        packet = self.build_packet(self.DXL_ID, self.ADDR_MX_GOAL_POSITION, position)
+        self.get_logger().info(f'Sending packet: {packet.hex()}')
         self.send_packet(packet)
 
     def build_packet(self, dxl_id, address, value):
-        length = 5  # Length of the packet
-        checksum = ~(dxl_id + length + 3 + address + (value & 0xFF) + (value >> 8) & 0xFF) & 0xFF
+        length = 5
+        checksum = ~(dxl_id + length + 3 + address + (value & 0xFF) + ((value >> 8) & 0xFF)) & 0xFF
         packet = bytearray([
-            0xFF, 0xFF,  # Header
+            0xFF, 0xFF,
             dxl_id,
             length,
-            3,  # Write instruction
+            3,
             address,
             value & 0xFF,
             (value >> 8) & 0xFF,
-            checksum
+            checksum & 0xFF
         ])
         return packet
 
     def send_packet(self, packet):
-        # Set direction to transmit
         GPIO.output(self.direction_pin, GPIO.HIGH)
-        
-        # Write to the serial port
         self.serial_port.write(packet)
         self.serial_port.flush()
-        
-        # Set direction to receive
         GPIO.output(self.direction_pin, GPIO.LOW)
-        
-        # Read response (if necessary)
         response = self.serial_port.read(6)
         self.get_logger().info(f'Response: {response.hex()}')
+        self.parse_response(response)
 
-    def get_position(self):
-        # Create a packet to read the present position
-        packet = self.build_read_packet(self.DXL_ID, self.ADDR_MX_PRESENT_POSITION, 2)
+    def parse_response(self, response):
+        if len(response) < 6:
+            self.get_logger().error('Incomplete response received')
+            return
         
-        # Send the packet
-        self.send_packet(packet)
+        header = response[:2]
+        dxl_id = response[2]
+        length = response[3]
+        error = response[4]
+        checksum = response[5]
 
-        # Read the position
-        response = self.serial_port.read(8)
-        if len(response) == 8:
-            position = response[5] + (response[6] << 8)
-            return position
-        else:
-            self.get_logger().error("Failed to get position")
-            return None
+        if header != b'\xff\xff':
+            self.get_logger().error('Invalid header')
+            return
 
-    def build_read_packet(self, dxl_id, address, length):
-        checksum = ~(dxl_id + 4 + 2 + address + length) & 0xFF
-        packet = bytearray([
-            0xFF, 0xFF,  # Header
-            dxl_id,
-            4,
-            2,  # Read instruction
-            address,
-            length,
-            checksum
-        ])
-        return packet
+        if error != 0:
+            self.get_logger().error(f'Error in response: {error:08b}')
+            self.log_error_details(error)
 
-    def timer_callback(self):
-        # Example: set servo position to 200 (move to another position)
-        self.set_position(430)
-        time.sleep(1)
-        self.set_position(712)  # Set position to 512 (center position)
-        time.sleep(1)
-
-        # Publish the updated position of the servo
-        position = self.get_position()
-        msg = Float64()
-        msg.data = position if position is not None else -1
-        self.pub.publish(msg)
+    def log_error_details(self, error):
+        errors = {
+            0x01: "Input Voltage Error",
+            0x02: "Angle Limit Error",
+            0x04: "Overheating Error",
+            0x08: "Range Error",
+            0x10: "Checksum Error",
+            0x20: "Overload Error",
+            0x40: "Instruction Error",
+        }
+        for bit, message in errors.items():
+            if error & bit:
+                self.get_logger().error(message)
 
 def main(args=None):
     rclpy.init(args=args)
     dynamixel_controller = DynamixelController()
-
-    try:
-        rclpy.spin(dynamixel_controller)
-    except KeyboardInterrupt:
-        pass
-
+    rclpy.spin(dynamixel_controller)
     dynamixel_controller.destroy_node()
     rclpy.shutdown()
 
