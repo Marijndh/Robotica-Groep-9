8e3e0f2fafa71a6e17efd5de74db4370ce90fd46
Marijndh <marijn.den.hollander@student.nhlstenden.com>
2024-05-28 20:40:54 +0200

    children gevonden, begonnen met kleur herkennen in vorm

--- Python/Robotica-Vision/ColorFiltering.py
+++ Python/Robotica-Vision/ColorFiltering.py
@@ -21,10 +21,10 @@ class Color:
 
 # Predefined colors with their HSV ranges
 colors = {
-    "red": Color("red", np.array([0, 200, 0]), np.array([10, 255, 255])),
+    "red": Color("red", np.array([0, 200, 0]), np.array([20, 255, 255])),
     "green": Color("green", np.array([40, 0, 0]), np.array([70, 255, 255])),
     "blue": Color("blue", np.array([90, 50, 50]), np.array([140, 255, 255])),
-    "pink": Color("pink", np.array([175, 50, 0]), np.array([180, 255, 255])),
+    "pink": Color("pink", np.array([170, 0, 0]), np.array([180, 255, 255])),
 }
 
 # List of primary and intermediate colors
@@ -33,7 +33,7 @@ primary_colors = [
 ]
 
 # Load and process frame
-frame = cv.imread('Images/kleuren_tangen.jpg')
+frame = cv.imread('Images/roze-tang.jpeg')
 frame = cv.resize(frame, (640, 640))
 hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
@@ -50,7 +50,6 @@ def display_individual_colors():
             cv.destroyAllWindows()
         else:
             print(f"Color {color_name} is not recognized")
-
 
 # Function to display all colors in one image with outlines and labels
 def display_all_colors():
@@ -69,17 +68,21 @@ def display_all_colors():
 
             contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL,
                                           cv.CHAIN_APPROX_SIMPLE)
+            result = []
             for contour in contours:
-                cv.drawContours(result_frame, [contour], -1, (0, 0, 0), 2)
-                M = cv.moments(contour)
-                if M["m00"] != 0:
-                    cX = int(M["m10"] / M["m00"])
-                    cY = int(M["m01"] / M["m00"])
-                else:
-                    cX, cY = 0, 0
-                cv.putText(result_frame, color.name, (cX - 20, cY),
-                           cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2,
-                           cv.LINE_AA)
+                if cv.contourArea(contour) > 1000:
+                    result.append(contour)
+                    M = cv.moments(contour)
+                    if M["m00"] != 0:
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+                    else:
+                        cX, cY = 0, 0
+                    cv.putText(result_frame, color.name, (cX - 20, cY),
+                               cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2,
+                               cv.LINE_AA)
+            cv.drawContours(result_frame, result, -1, (0, 0, 0), 2)
+            print(color_name + ':' + str(len(result)))
 
     if combined_mask is not None:
         combined_res = cv.bitwise_and(frame, frame, mask=combined_mask)
@@ -89,9 +92,9 @@ def display_all_colors():
         cv.destroyAllWindows()
 
 # Display individual colors
-display_individual_colors()
+#display_individual_colors()
 
 # Display all colors in one image
-display_all_colors()
+#display_all_colors()
 
-cv.destroyAllWindows()
+#cv.destroyAllWindows()
--- Python/Robotica-Vision/FindInstrumentEyes.py
+++ Python/Robotica-Vision/FindInstrumentEyes.py
@@ -1,39 +1,68 @@
 import numpy as np
 import cv2 as cv
-import math
+from ColorFiltering import Color, colors, primary_colors
+
+frame = cv.imread('Images/blauwe-tang.jpeg')
+frame = cv.resize(frame, (640, 640))
+imgray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
 
 class Instrument:
     def __init__(self, index, body):
         self.index = index
         self.body = body
+        self.color = None
         self.children = []
 
     def add_child(self, child):
         self.children.append(child)
 
+    def set_color(self, color):
+        self.color = color
+
 
 def get_instrument_centroid(instrument):
     # compute the center of the contour
-    M = cv.moments()
+    M = cv.moments(instrument.body)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
     # draw the contour and center of the shape on the image
-    cv.drawContours(frame, [c], -1, (0, 255, 0), 2)
     cv.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
 
 
-# noinspection PyShadowingNames
 def draw_instruments(frame, instruments):
     for i in instruments:
         objects = i.children
         objects.append(i.body)
-        cv.drawContours(frame, objects, -1, (0, 0, 0), 3)
+        cv.drawContours(frame, objects, -1, (0, 255, 0), 3)
+
+#https://cullensun.medium.com/agglomerative-clustering-for-opencv-contours-cd74719b678e
+def get_instrument_colors(frame, instruments):
+    combined_mask = None
+    colored_contours = []
+    for color_name in primary_colors:
+        if color_name in colors:
+            color = colors[color_name]
+            mask = color.get_mask(hsv)
+
+            if combined_mask is None:
+                combined_mask = mask
+            else:
+                combined_mask = combined_mask | mask
+
+            contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL,
+                                          cv.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours:
+                if cv.contourArea(contour) > 1000:
+                    colored_contours.append([color_name, contour])
+                    cv.drawContours(frame, [contour], -1, (255, 0, 0), 2)
+    for i in instruments:
+        for j in colored_contours:
+            print(cv.matchShapes(i.body, j[1], cv.CONTOURS_MATCH_I3, 0.0))
 
 
-frame = cv.imread('Images/blauwe-tang.jpeg')
-frame = cv.resize(frame, (640, 640))
-imgray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 _, thresh = cv.threshold(imgray, 80, 100, cv.THRESH_BINARY)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 hierarchy = hierarchy[0]
@@ -51,6 +80,7 @@ for i in instruments:
     for j in range(len(hierarchy)):
         if hierarchy[j][3] == i.index and cv.contourArea(contours[j]) > 1000:
             i.add_child(contours[j])
+get_instrument_colors(frame, instruments)
 draw_instruments(frame, instruments)
 cv.imshow('Contours', frame)
 cv.waitKey(0)
