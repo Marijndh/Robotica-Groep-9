29a056406d293978ed04b4338e5d9d4033b11a0b
berend109 <berend109@gmail.com>
2024-05-28 11:29:20 +0200

    pep standaard jongens

--- Python/Robotica-Vision/FindInstrumentEyes.py
+++ Python/Robotica-Vision/FindInstrumentEyes.py
@@ -2,18 +2,21 @@ import numpy as np
 import cv2 as cv
 import math
 import Color
+
+
 def test():
     frame = cv.imread('Images/roze-tang.jpeg')
     frame = cv.resize(frame, (640, 640))
     imgray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    _, thresh = cv.threshold(imgray,150,255,cv.THRESH_BINARY)
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    _, thresh = cv.threshold(imgray, 150, 255, cv.THRESH_BINARY)
+    contours, hierarchy = cv.findContours(thresh,
+                                          cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     result = []
     for cnr in range(len(contours)):
         cnt = contours[cnr]
         area = cv.contourArea(cnt)
-        #perimeter = cv.arcLength(cnt, True)
-        #factor = 4 * math.pi * area / perimeter ** 2
+        # perimeter = cv.arcLength(cnt, True)
+        # factor = 4 * math.pi * area / perimeter ** 2
         if 5000.0 < area < 400000:
             print(str(cnr)+": "+str(area))
             result.append(cnt)
