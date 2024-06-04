bbd2da57da702455bdb0f326b212e443fa225ce9
berend109 <berend109@gmail.com>
2024-06-04 17:12:41 +0200

    tying to place bullseye on the grid location

--- Python/Robotica-Vision/FindTargets.py
+++ Python/Robotica-Vision/FindTargets.py
@@ -4,6 +4,10 @@ import numpy as np
 from ColorFiltering import Color
 from create_grid_on_frame import create_grid_on_image
 
+width_img = 1080
+height_img = 720
+location_bullseye_cells = []
+
 # Functie om te controleren of een contour ellipsvormig is
 def is_ellipse(contour, min_aspect_ratio=0.5):
     if len(contour) < 5:
@@ -13,31 +17,52 @@ def is_ellipse(contour, min_aspect_ratio=0.5):
     aspect_ratio = min(MA, ma) / max(MA, ma)
     return aspect_ratio >= min_aspect_ratio, (int(x), int(y))
 
+
 def find_bulls_eyes(inv_contours):
     result = []
     for contour in inv_contours:
         is_ellipse_flag, center = is_ellipse(contour)
         if is_ellipse_flag:
+            print(center, "center and flag: ", is_ellipse_flag)
             ellipse = cv.fitEllipse(contour)
 
-            # Maak een masker voor de cirkel
-            mask = np.zeros(gray_img.shape, dtype=np.uint8)
-            cv.circle(mask, center, 5, (255, 255, 255), -1)  # Aangenomen straal van 10 pixels
+            mask = create_mask(center)
 
             # Bereken het gemiddelde HSV in de cirkel
             hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
             mean = cv.mean(hsv_img, mask=mask)
-            print(mean)
-            bulls_eye = Color('bulls_eye', np.array([20, 0, 0]), np.array([25, 255, 255]))
-            if bulls_eye.is_color(mean[0], mean[1], mean[2]):
-                cv.circle(img, center, 5, (0, 0, 0), -1)
-                cv.ellipse(img, ellipse, (0, 255, 0), 2)
-                result.append(center)
+            # print(mean)
+            color_bulls_eye(center, ellipse, mean, result)
     return result
+
+
+def color_bulls_eye(center, ellipse, mean, result):
+    bulls_eye = Color('bulls_eye', np.array([20, 0, 0]), np.array([25, 255, 255]))
+    if bulls_eye.is_color(mean[0], mean[1], mean[2]):
+        cv.circle(img, center, 5, (0, 0, 0), -1)
+        cv.ellipse(img, ellipse, (0, 255, 0), 2)
+        result.append(center)
+        print('Bulls eye found at: ' + center.__str__())
+
+
+# def check_bulls_eye_in_grid(bulls_eyes, grid_centers):
+#     bulls_eye_cells = []
+#     for center in bulls_eyes:
+#         for cell in grid_centers:
+#             if abs(center[0] - cell[0]) <= cell_size_width // 2 and abs(center[1] - cell[1]) <= cell_size_height // 2:
+#                 bulls_eye_cells.append(cell)
+#     return bulls_eye_cells
+
+def create_mask(center):
+    # Maak een masker voor de cirkel
+    mask = np.zeros(gray_img.shape, dtype=np.uint8)
+    cv.circle(mask, center, 5, (255, 255, 255), -1)  # Aangenomen straal van 10 pixels
+    return mask
+
 
 # Lees de afbeelding in
 img = cv.imread('Images/bulls-eye.jpg')
-img = cv.resize(img, (1080, 720))
+img = cv.resize(img, (width_img, height_img))
 
 # Converteer de afbeelding naar grijswaarden
 gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -53,11 +78,11 @@ contours, _ = cv.findContours(thresh_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMP
 
 bulls_eyes = find_bulls_eyes(contours)
 
-print('Bull_eyes locations:'+ bulls_eyes.__str__())
-
+# print('Bull_eyes locations:' + bulls_eyes.__str__())
 
 # Toon de resultaten
 cv.imshow('Origineel met Ellipsen', img)
-cv.imshow("grid", create_grid_on_image(img, 40, 60))
+# round omdat je int nodig hebt bij create_grid_on_image
+cv.imshow("grid", create_grid_on_image(img, round(width_img / 2), round(height_img / 2)))
 cv.waitKey(0)
 cv.destroyAllWindows()
--- Python/Robotica-Vision/create_grid_on_frame.py
+++ Python/Robotica-Vision/create_grid_on_frame.py
@@ -11,4 +11,5 @@ def create_grid_on_image(frame, num_cells_width, num_cells_height):
         cv.line(frame, (i * cell_size_width, 0), (i * cell_size_width, height), (0, 0, 0), 1)
     for i in range(0, num_cells_height):
         cv.line(frame, (0, i * cell_size_height), (width, i * cell_size_height), (0, 0, 0), 1)
+
     return frame
