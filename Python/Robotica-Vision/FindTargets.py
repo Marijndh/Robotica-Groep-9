8f1308351a0d3f7db99455d883c13fbe86e73663
berend109 <berend109@gmail.com>
2024-06-05 17:25:05 +0200

    format

--- Python/Robotica-Vision/FindTargets.py
+++ Python/Robotica-Vision/FindTargets.py
@@ -8,6 +8,7 @@ width_img = 1080
 height_img = 720
 location_bullseye_cells = []
 
+
 # Functie om te controleren of een contour ellipsvormig is
 def is_ellipse(contour, min_aspect_ratio=0.5):
     if len(contour) < 5:
@@ -37,7 +38,8 @@ def find_bulls_eyes(inv_contours):
 
 
 def color_bulls_eye(center, ellipse, mean, result):
-    bulls_eye = Color('bulls_eye', np.array([20, 0, 0]), np.array([25, 255, 255]))
+    bulls_eye = Color('bulls_eye', np.array([20, 0, 0]),
+                      np.array([25, 255, 255]))
     if bulls_eye.is_color(mean[0], mean[1], mean[2]):
         cv.circle(img, center, 5, (0, 0, 0), -1)
         cv.ellipse(img, ellipse, (0, 255, 0), 2)
@@ -45,18 +47,20 @@ def color_bulls_eye(center, ellipse, mean, result):
         print('Bulls eye found at: ' + center.__str__())
 
 
-# def check_bulls_eye_in_grid(bulls_eyes, grid_centers):
-#     bulls_eye_cells = []
-#     for center in bulls_eyes:
-#         for cell in grid_centers:
-#             if abs(center[0] - cell[0]) <= cell_size_width // 2 and abs(center[1] - cell[1]) <= cell_size_height // 2:
-#                 bulls_eye_cells.append(cell)
-#     return bulls_eye_cells
+def check_bulls_eye_in_grid(bulls_eyes):
+    bulls_eye_cells = []
+    for center in bulls_eyes:
+        for cell in grid_centers:
+            if abs(center[0] - cell[0]) <= cell_size_width // 2 and abs(center[1] - cell[1]) <= cell_size_height // 2:
+                bulls_eye_cells.append(cell)
+    return bulls_eye_cells
+
 
 def create_mask(center):
     # Maak een masker voor de cirkel
     mask = np.zeros(gray_img.shape, dtype=np.uint8)
-    cv.circle(mask, center, 5, (255, 255, 255), -1)  # Aangenomen straal van 10 pixels
+    # Aangenomen straal van 10 pixels
+    cv.circle(mask, center, 5, (255, 255, 255), -1)
     return mask
 
 
@@ -74,7 +78,11 @@ blurred_img = cv.GaussianBlur(gray_img, (9, 9), 2)
 ret, thresh_img = cv.threshold(blurred_img, 127, 255, cv.THRESH_BINARY_INV)
 
 # Detecteer contouren in de inverse afbeelding
-contours, _ = cv.findContours(thresh_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+contours, _ = cv.findContours(
+        thresh_img,
+        cv.RETR_EXTERNAL,
+        cv.CHAIN_APPROX_SIMPLE
+        )
 
 bulls_eyes = find_bulls_eyes(contours)
 
@@ -83,6 +91,7 @@ bulls_eyes = find_bulls_eyes(contours)
 # Toon de resultaten
 cv.imshow('Origineel met Ellipsen', img)
 # round omdat je int nodig hebt bij create_grid_on_image
-cv.imshow("grid", create_grid_on_image(img, round(width_img / 2), round(height_img / 2)))
+cv.imshow("grid", create_grid_on_image(img, round(width_img / 2),
+                                       round(height_img / 2)))
 cv.waitKey(0)
 cv.destroyAllWindows()
