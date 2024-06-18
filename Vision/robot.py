28eb86d6b35328c3515a0c6fc302c399a42197d2
Marijndh <marijn.den.hollander@student.nhlstenden.com>
2024-06-18 20:19:31 +0200

    Update robot.py

--- Vision/robot.py
+++ Vision/robot.py
@@ -6,6 +6,7 @@ import requests
 import json
 import socket
 
+
 class Robot:
     def __init__(self):
         self.color = None
@@ -37,25 +38,52 @@ class Robot:
             mode = data.get('mode', 'N/A')
             self.mode = mode
 
-    def move_to_location(self):
+    def send_command(self, x, y, z, r, gripper):
+        # De URL van de server
+        url = "http://" + self.IP + ":5000/servo/kinematics"
+
+        data = str(x) + " " + str(y)
+        # De payload (gegevens) die we willen versturen
+        data = {"command": data}
+
+        # De headers die we willen versturen
+        headers = {"Content-Type": "application/json"}
+
+        # Verstuur het POST-verzoek
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        return response.status_code
+
+    def move_to_instrument(self):
         if self.target_point != (0, 0):
             self.location = self.target_point
             result = GeometryUtils.map_coordinate(self.target_point)
-
             self.target_point = (0, 0)
-            # De URL van de server
-            url = "http://"+ self.IP +":5000/servo/kinematics"
 
-            data = str(result[0]) + " " + str(result[1])
-            # De payload (gegevens) die we willen versturen
-            data = {"command": data}
+            #lower z-axes to 13
+            
+            # move to instrument
 
-            # De headers die we willen versturen
-            headers = {"Content-Type": "application/json"}
+            # open gripper and grab object
 
-            # Verstuur het POST-verzoek
-            response = requests.post(url, headers=headers, data=json.dumps(data))
+            # increase z-axes to 20
 
-            # Controleer de respons
-            if response.status_code == 200:
-                print("Bewogen naar: " + str(result))
+            # move to instrument location, involve instrument.type
+
+            #lower z-axes to 13
+
+            #release gripper
+
+            # move to new target or 600, 0
+
+    def move_to_target(self):
+        if self.target_point != (0, 0):
+            self.location = self.target_point
+            result = GeometryUtils.map_coordinate(self.target_point)
+            self.target_point = (0, 0)
+
+            #move to target
+
+            #lower z-axes to tap target
+
+            #increase z-axes to around 17
